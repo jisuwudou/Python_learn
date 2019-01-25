ea769c237c2c4aab02a45c027a232423d121faf4
Mas <932537063@qq.com>
2019-01-25 20:58:39 +0800

    1

--- media/douyin_dl1/tes.py
+++ media/douyin_dl1/tes.py
@@ -1,15 +1,17 @@
 import requests
 import urllib3
 import time
+import os
 urllib3.disable_warnings()
-response = requests.get('https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200fc80000beb5rlqgd9ft9jma9lkg&line=0',verify=False)
+# response = requests.get('https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200fc80000beb5rlqgd9ft9jma9lkg&line=0',verify=False)
 
 size = 0
 chunk_size = 1024
 counter = 1
-with open(r'urlsfile.txt') as urlsfile:
+with open(r'cat.txt') as urlsfile:
     while True:
         url = urlsfile.readline()
+        # print(url)
         if not url:
             break
 
@@ -17,7 +19,7 @@ with open(r'urlsfile.txt') as urlsfile:
             response = requests.get(
                 url,
                 verify=False)
-            filename = 'cailuoli/'+ str(int( time.time())) + '.mp4'
+            filename = 'cat/'+ str( time.time()) + '.mp4'
             with open(filename, "wb") as file:
                 for data in response.iter_content(chunk_size=chunk_size):
                     file.write(data)
@@ -25,6 +27,9 @@ with open(r'urlsfile.txt') as urlsfile:
                     file.flush()
             print('finish='+str(counter))
             counter = counter + 1
+            if counter > 885:
+                os.system('shutdown -s -t 10')
+                break
         except Exception as e:
             print(e)
     # print(url+" ")
